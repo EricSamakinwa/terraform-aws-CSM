7f4a9fd557be7b0df94d527433c06299099ac5b9
Eric Samakinwa <57706825+shamakson@users.noreply.github.com>
2022-08-29 14:12:42 +0200

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -11,25 +11,4 @@ terraform {
     }
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "CSM-s3-bucket"
-  
-  lifecycle {
-      prevent_destroy = true
-  }
-  
-  versioning {
-      enabled = true
-  }
-    server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "AES256"
-      }
-    }
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
